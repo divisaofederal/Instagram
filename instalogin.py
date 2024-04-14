1b4393a9d567c1bb7615c5444ed25e3b3511dd62
divisaofederal <166842966+divisaofederal@users.noreply.github.com>
2024-04-14 16:00:23 -0300

    Add files via upload

--- instalogin.py
+++ instalogin.py
@@ -1,14 +1,9 @@
-# All rights reserved to Seyzalel. Reproduction in whole or in part without prior permission is prohibited. © Seyzalel 2024
-
-import time
-import random
 from selenium.common.exceptions import TimeoutException
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
@@ -22,29 +17,33 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-extensions')
 chrome_options.add_argument('--disable-web-security') 
 chrome_options.add_argument('--allow-running-insecure-content')
+
 driver = webdriver.Chrome(options=chrome_options)
 
 try:
-  driver.get("https://instagram.com/")
-  entrar = WebDriverWait(driver, 10).until(
+    driver.get("https://www.instagram.com/")
     
-    EC.element_to_be_clickable((By.XPATH, '//div[contains(text(), "Entrar")]'))
-  )
-  entrar.click()
-  
-  input_username = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.XPATH, '//input[@name="username"]'))
-  )
-  if input_username:
-    print("O botão foi clicado com sucesso e o input foi encontrado.")
-  else:
-    print("Falha ao clicar no botão ou o input não foi encontrado.")
+    # Espera até que o botão "Entrar" seja clicável
+    entrar = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//div[@class="_ap3a _aaco _aacw _aad0 _aad6"][contains(text(), "Entrar")]'))
+    )
+    entrar.click()
+    
+    # Espera até que o campo de nome de usuário seja visível
+    input_username = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//input[@name="username"]'))
+    )
+    
+    if input_username:
+        print("O botão foi clicado com sucesso e o input foi encontrado.")
+    else:
+        print("Falha ao clicar no botão ou o input não foi encontrado.")
     
 except TimeoutException:
-  print("Tempo limite excedido ao aguardar o botão ou o input.")
-
+    print("Tempo limite excedido ao aguardar o botão ou o input.")
+    
 except Exception as e:
-  print("Ocorreu um erro:", e)
-
+    print("Ocorreu um erro:", e)
+    
 finally:
-  driver.quit()
+    driver.quit()
