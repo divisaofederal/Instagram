8b897e46d3a585ba2a27a5a49e0b6d5821b2b270
divisaofederal <166842966+divisaofederal@users.noreply.github.com>
2024-04-14 17:52:01 -0300

    Add files via upload

--- instalogin.py
+++ instalogin.py
@@ -22,49 +22,25 @@ chrome_options.add_argument('--allow-running-insecure-content')
 
 driver = webdriver.Chrome(options=chrome_options)
 
-# Acessar a página de login
+# Acessa a página de login do Instagram
 driver.get("https://www.instagram.com/accounts/login/")
 
-# Esperar até que o campo de nome de usuário esteja presente na página
-username_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='username']")))
-
-# Inserir usuário com um atraso e movimento de mouse
-for char in "seyzalel":
-    username_input.send_keys(char)
-    time.sleep(random.uniform(0.1, 0.3))
-    ActionChains(driver).move_by_offset(random.uniform(1, 5), random.uniform(1, 5)).perform()
-
-# Esperar um pouco antes de inserir a senha
-time.sleep(random.uniform(1, 2))
-
-# Inserir senha com um atraso e movimento de mouse
-password_input = driver.find_element(By.CSS_SELECTOR, "input[name='password']")
-for char in "Sey17zalel17@$":
-    password_input.send_keys(char)
-    time.sleep(random.uniform(0.1, 0.3))
-    ActionChains(driver).move_by_offset(random.uniform(1, 5), random.uniform(1, 5)).perform()
-
-# Esperar um pouco antes de clicar no botão de login
-time.sleep(random.uniform(1, 2))
-
-# Localizar e clicar no botão de login com um pequeno desvio no movimento do mouse
-login_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-ActionChains(driver).move_to_element(login_button).perform()
-time.sleep(random.uniform(0.5, 1))
-login_button.click()
-
-# Acessar a página do perfil específico
-driver.get("https://www.instagram.com/abra_paola/")
-
-# Esperar até que o ícone de opções esteja presente na página do perfil
 try:
-    options_icon = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "svg[aria-label='Opções']")))
-    print("Acessou a página com sucesso.")
-except:
-    print("Erro: A página não foi carregada corretamente ou o elemento não está presente.")
+  # Verifica se o elemento username existe
+  username_field = WebDriverWait(driver, 10).until(
+      EC.presence_of_element_located((By.NAME, "username"))
+  )
+  # Verifica se o elemento password existe
+  password_field = WebDriverWait(driver, 10).until(
+      EC.presence_of_element_located((By.NAME, "password"))
+  )
 
-# Aguardar um tempo aleatório antes de fechar o navegador
-time.sleep(random.uniform(3, 5))
+  # Imprime mensagem de sucesso
+  print("Página acessada com sucesso!")
 
-# Fechar o navegador
-driver.quit()
+except TimeoutException:
+  print("Elementos de login não encontrados!")
+
+finally:
+  # Fecha o navegador
+  driver.quit()
