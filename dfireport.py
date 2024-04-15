017e22a5ac4c888f98ce8b7e909d0b20c684fbce
divisaofederal <166842966+divisaofederal@users.noreply.github.com>
2024-04-14 23:38:08 -0300

    Add files via upload

--- dfireport.py
+++ dfireport.py
@@ -20,17 +20,17 @@ chrome_options.add_argument('--allow-running-insecure-content')
 
 driver = webdriver.Chrome(options=chrome_options)
 
-# Acessar a página do Help Center do Instagram
-driver.get("https://help.instagram.com/")
+# Acessar a página específica do Help Center do Instagram
+driver.get("https://help.instagram.com/contact/723586364339719")
 
-# Esperar até que o título da página contenha "Help Center"
-WebDriverWait(driver, 10).until(EC.title_contains("Help Center"))
+# Esperar até que o título da página contenha "Central de Ajuda do Instagram"
+WebDriverWait(driver, 10).until(EC.title_contains("Central de Ajuda do Instagram"))
 
-# Verificar se o título da página contém "Help Center"
-if "Help Center" in driver.title:
+# Verificar se o título da página contém "Central de Ajuda do Instagram"
+if "Central de Ajuda do Instagram" in driver.title:
     print("Acessou a página com sucesso.")
 else:
-    print("Erro: O título da página não contém 'Help Center'.")
+    print("Erro: O título da página não contém 'Central de Ajuda do Instagram'.")
 
 # Fechar o navegador
 driver.quit()
